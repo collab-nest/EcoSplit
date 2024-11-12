eb9572d3c401765d4ad1563354787181a7591497
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-11-12 10:56:51 +0100

    Setting up Spring Boot Project

---
+++ server/src/main/java/com/collabnest/ecosplit/EcosplitApplication.java
@@ -0,0 +1,13 @@
+package com.collabnest.ecosplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcosplitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcosplitApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/collabnest/ecosplit/EcosplitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collabnest.ecosplit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcosplitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
